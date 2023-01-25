c0d1fd683e51753e451d85bb11b7fb70dfedccee
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2023-01-24 19:47:24 -0500

    first push

---
+++ server/api/index.js
@@ -0,0 +1,6 @@
+const router = require("express").Router();
+
+router.use("/", require("./api"));
+
+module.exports = router;
+
---
+++ server/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+
+app.use(express.json());
+
+app.use(express.urlencoded({extended:false}));
+
+app.use("/api", require('./api'));
+
+app.use((error, req, res, next) =>{
+    console.error(error);
+    res.status(error.status).send(error.message);
+})
+
+module.exports = app;
