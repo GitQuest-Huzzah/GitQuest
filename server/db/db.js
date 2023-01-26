6fec1e05cee3921cc424c5af148f7c2efd36dafe
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2023-01-26 18:27:08 -0500

    gcp cloud

--- server/db/db.js
+++ server/db/db.js
@@ -1,9 +1,24 @@
 const Sequelize = require("sequelize");
 
 //create database link between sequelize and PG
-const db = new Sequelize("postgres://localhost:5432/gitGoingSlackBot", {
-	logging: false,
-});
+// const db = new Sequelize("postgres://34.27.41.97/gitgoingdb", {
+// 	logging: false,
+// });
+const db = new Sequelize({
+	database: "postgres",
+	username: "postgres",
+	password: `***REMOVED***`,
+	host: "34.27.41.97",
+	port: 5432,
+	dialect: "postgres",
+	dialectOptions:{
+		ssl: {
+			require:true,
+			rejectUnauthorized:false
+		}
+	}
+
+  });
 
 db.sync()
 module.exports = db;
