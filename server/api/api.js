0081a9d9e51d1a0c4ef0cc913d2347f628cb43c7
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2023-01-26 02:01:21 -0500

    not sure wtf is wrong with the logging when deployed

--- server/api/api.js
+++ server/api/api.js
@@ -1,23 +1,9 @@
-const writeFileToBucket = require("./logging");
+// const writeFileToBucket = require("./logging");
 
 const router = require("express").Router();
 
-router.get("/", async (req,res,next)=> {
-    const storage = new Storage();
-        try {
-          const results = await storage.getBuckets();
-      
-          const [buckets] = results;
-      
-          console.log('Buckets:');
-          buckets.forEach(bucket => {
-            console.log(bucket.name);
-            res.json(results)
-          });
-        } catch (err) {
-          console.error('ERROR:', err);
-        }
-   
+router.get("/", (req,res,next)=> {
+    res.json({helloWorld: "hello world"})
     // writeFileToBucket(req)
 })
 
