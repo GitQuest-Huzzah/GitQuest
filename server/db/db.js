5fe08d3f88c9345a19bcdfd7d44579b0ad836fbb
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2023-01-26 21:38:15 -0500

    attempted to connect to gcp with sequelize

--- server/db/db.js
+++ server/db/db.js
@@ -9,5 +9,13 @@ const db = new Sequelize("postgres", "postgres", `***REMOVED***`, {
 	},
 });
 
+db.authenticate().then(() => {
+	console.log('Connection established successfully.');
+  }).catch(err => {
+	console.error('Unable to connect to the database:', err);
+  }).finally(() => {
+	db.close();
+  });
+
 db.sync();
 module.exports = db;
