79a3cfd83a9a3ba6fb578966256f09a34700a9e8
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2023-01-27 14:02:37 -0500

    testing out query logging

--- server/slackFuncs/commands.js
+++ server/slackFuncs/commands.js
@@ -2,35 +2,38 @@ const { WebClient } = require("@slack/web-api");
 const web = new WebClient();
 
 const gitWorkFlow = async (reqBody, res) => {
-    console.log("This is the req body", reqBody);
-    await web.chat.postMessage({
-        text: "Hello world",
-        channel: reqBody.user_id,
-        token: "***REMOVED***",
-    });
+	console.log("This is the req body", reqBody);
+	await web.chat.postMessage({
+		text: "Hello world",
+		channel: reqBody.user_id,
+		token: "***REMOVED***",
+	});
 };
 
 const blockTest = async (reqBody, res) => {
-    await web.chat.postMessage({
-        blocks: [
-            {
-                type: "section",
-                text: {
-                    type: "plain_text",
-                    text: "This is a plain text section block.",
-                    emoji: true,
-                },
-            },
-        ],
-        channel: reqBody.user_id,
-        token: "***REMOVED***",
-    });
+	await web.chat.postMessage({
+		blocks: [
+			{
+				type: "section",
+				text: {
+					type: "plain_text",
+					text: "This is a plain text section block.",
+					emoji: true,
+				},
+			},
+		],
+		channel: reqBody.user_id,
+		token: "***REMOVED***",
+	});
 };
 
-
-const slackInstallAuth = async (req, res)=>{
-    console.log(req.query,"this is the req query")
-    web.oauth.v2.access({code:req.query.code, client_id:"***REMOVED***", client_secret: "***REMOVED***",})
-}
-module.exports = {gitWorkFlow, slackInstallAuth, blockTest }
-
+const slackInstallAuth = async (req, res) => {
+	console.log(req.query, "this is the req query");
+	const accessFunction = web.oauth.v2.access({
+		code: req.query.code,
+		client_id: "***REMOVED***",
+		client_secret: "***REMOVED***",
+	});
+    console.log(accessFunction, "this is the access v2 attempt")
+};
+module.exports = { gitWorkFlow, slackInstallAuth, blockTest };
