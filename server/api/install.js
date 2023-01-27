8a5103622f2eb6d1104502f507e896168d538b7d
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2023-01-27 13:54:49 -0500

    testing out query logging

--- server/api/install.js
+++ server/api/install.js
@@ -1,12 +1,45 @@
 const { slackInstallAuth } = require("../slackFuncs/commands");
+const { InstallProvider } = require("@slack/oauth");
 
 const router = require("express").Router();
 
-router.get("/", (req,res,next)=> {
-    res.sendStatus(200)
-    console.log(req.query,"route was hit query logged")
-    slackInstallAuth(req)
-})
+// const installer = new InstallProvider({
+// 	clientId: "4706667577361.4706675007441",
+// 	clientSecret: "9bfcc36a2b43abce2e9dfc6b108cbb24",
+// 	// stateSecret: "bobbyBIsGoingToGetBusy",
+// });
 
+//path is /api/slack/install
+router.get("/", async (req, res, next) => {
+	// await installer.handleInstallPath(req, res, {
+	// 	scopes: [
+	// 		"channels:join",
+	// 		"channel:read",
+	// 		"chat:write",
+	// 		"chat:write.public",
+	// 		"commands",
+	// 		"groups:read",
+	// 		"im:read",
+	// 		"im:write",
+	// 		"incoming-webhook",
+	// 		"links:read",
+	// 		"links:write",
+	// 		"team:read",
+	// 		"users:read",
+	// 		"users:read.email",
+	// 		"users:write",
+	// 	],
+    //     userScopes:['identity.email'],
+    //     redirectUri:"https://gitgoingslackbot.uc.r.appspot.com/api/slack/install/redirect"
+	// });
+	res.sendStatus(200);
+	// console.log(req.query, "route was hit query logged");
+	// slackInstallAuth(req);
+});
+//path is /api/slack/install/redirect
+router.get("/redirect", (req,res) =>{
+    // installer.handleCallback(req,res)
+    slackInstallAuth(req);
+});
 
 module.exports = router;
