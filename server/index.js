51257afce118606feae1c2d3e5f3fc31b9b2e18a
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2023-01-26 19:10:42 -0500

    attempted to connect to gcp with sequelize

--- server/index.js
+++ server/index.js
@@ -2,10 +2,10 @@ const {app} = require('./app');
 const Bots = require('./db/models/Bots');
 const port = 8080;
 
-(async () => await Bots.create({
+ Bots.create({
     token: "Token123",
     teamID: "teamID123",
     teamName: "teamName123"
-}))();
+})
 
 app.listen(process.env.PORT || port, ()=> console.log(`Listening ${process.env.PORT || port}`));
