f362bd453346751b9ed43305ea82415f41d70311
Bob <robert.dale.boles@gmail.com>
2023-01-27 13:44:47 -0700

    db sync should work

--- server/db/db.js
+++ server/db/db.js
@@ -2,18 +2,24 @@ const Sequelize = require("sequelize");
 
 // const db = new Sequelize('postgres://localhost:5432/gitgoingdb')
 const db = new Sequelize("postgres", "postgres", `***REMOVED***`, {
-	dialect: "postgres",
-	host: "***REMOVED***",
-	logging: true,
-	dialectOptions: {
-		socketPath: "***REMOVED***",
-	},
+    dialect: "postgres",
+    host: "***REMOVED***",
+    logging: false,
+    dialectOptions: {
+        socketPath: "***REMOVED***",
+    },
 });
 
+// db.authenticate()
+//     .then(() => {
+//         console.log("Connection has been established successfully.");
+//     })
+//     .catch((err) => {
+//         console.error("Unable to connect to the database:", err);
+//     });
 
 db.sync({force: true});
 
 // db.sync();
-
 
 module.exports = db;
--- server/slackFuncs/commands.js
+++ server/slackFuncs/commands.js
@@ -4,7 +4,7 @@ const web = new WebClient();
 const gitWorkFlow = async (reqBody, res) => {
     console.log("This is the req body", reqBody);
     await web.chat.postMessage({
-        text: "Hello world",
+        text: "Step 1. Gently, yet firmly, remove your head from your ass. Can you see? Excellent.",
         channel: reqBody.user_id,
         token: "***REMOVED***",
     });
@@ -26,11 +26,31 @@ const blockTest = async (reqBody, res) => {
         token: "***REMOVED***",
     });
 };
+const bangedMom = async (reqBody, res) => {
+    console.log("THIS IS THE REQBODY FOR BANGED MOM",reqBody)
+    await web.chat.postMessage({
+        blocks: [
+            {
+                type: "input",
+                element: {
+                    type: "number_input",
+                    is_decimal_allowed: false,
+                    action_id: "number_input-action",
+                },
+                label: {
+                    type: "plain_text",
+                    text: "Number of times you've banged Bob's Mom:",
+                    emoji: true,
+                },
+            },
+        ],
+        channel: reqBody.channel_id,
+        token: "***REMOVED***",
+    });
+};
 
-
-const slackInstallAuth = async (req, res)=>{
-    console.log(req.query,"this is the req query")
-    web.oauth.v2.access(req.query.code)
-}
-module.exports = {gitWorkFlow, slackInstallAuth, blockTest }
-
+const slackInstallAuth = async (req, res) => {
+    console.log(req.query, "this is the req query");
+    web.oauth.v2.access(req.query.code);
+};
+module.exports = { gitWorkFlow, slackInstallAuth, blockTest, bangedMom };
