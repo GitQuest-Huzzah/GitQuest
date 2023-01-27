b1b1f3f2fbe7c3b6d40b699cf22db172db1ed308
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2023-01-26 19:13:04 -0500

    attempted to connect to gcp with sequelize

--- server/app.js
+++ server/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 
-console.log("ENVIRONMENTAL VARS",process.env)
+
+
 app.use(express.json());
 
 app.use(express.urlencoded({extended:false}));
@@ -12,6 +13,12 @@ app.use((error, req, res, next) =>{
     console.error(error);
     res.status(error.status).send(error.message);
 })
+
+Bots.create({
+    token: "Token123",
+    teamID: "teamID123",
+    teamName: "teamName123"
+})  
 
 module.exports = {app};
 
