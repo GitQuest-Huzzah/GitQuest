a1d1d21caea065f63d87ab717e4a6fdbc8d5f8e1
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2023-01-26 21:15:22 -0500

    attempted to connect to gcp with sequelize

--- server/db/db.js
+++ server/db/db.js
@@ -1,21 +1,13 @@
 const Sequelize = require("sequelize");
-const Bots = require("./models/Bots");
 
-console.log("before sequelize")
-const db = new Sequelize("postgres", "postgres", `_"._&BY[-It';3.q`,{
+const db = new Sequelize("postgres", "postgres", `_"._&BY[-It';3.q`, {
 	dialect: "postgres",
 	host: "/cloudsql/gitgoingslackbot:us-central1:gitgoingdb",
-	logging:true,
-	dialectOptions:{
-		socketPath:"/cloudsql/gitgoingslackbot:us-central1:gitgoingdb"
-	}
-  });
-  
-  Bots.create({
-    token: "Token123",
-    teamID: "teamID123",
-    teamName: "teamName123"
-})
-console.log("after Sequelize")
-db.sync()
+	logging: true,
+	dialectOptions: {
+		socketPath: "/cloudsql/gitgoingslackbot:us-central1:gitgoingdb",
+	},
+});
+
+db.sync();
 module.exports = db;
