a6a68e5319d4c3cb563f132cfaebe16cc285bf58
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2023-01-28 01:51:27 -0500

    testing out git auth

--- server/api/github.js
+++ server/api/github.js
@@ -6,31 +6,37 @@ const githubClientSecret = "***REMOVED***";
 
 //path is /api/github/auth/connect
 router.get("/auth/connect", async (req, res, next) => {
-    console.log('connect route hit')
-	res.redirect(
-		`https://github.com/login/oauth/authorize?client_id=${githubClientId}&scope=read:repo_hook,read:org,read:user,read:email,read:discussion`
-	).status(200)
+	console.log("connect route hit");
+	res
+		.redirect(
+			`https://github.com/login/oauth/authorize?client_id=${githubClientId}&scope=read:repo_hook,read:org,read:user,read:email,read:discussion`
+		)
+		.status(200);
 });
 
 let token = null;
 //path is /api/github/auth/redirect
 router.get("/auth/redirect", (req, res, next) => {
-    console.log('redirect route hit this is the query', req.query)
+	console.log("redirect route hit this is the query", req.query);
 	const body = {
 		client_id: githubClientId,
 		client_secret: githubClientSecret,
 		code: req.query.code,
 	};
 	const opts = { headers: { accept: "application/json" } };
-	axios
-		.post(`https://github.com/login/access_token`, body, opts)
-		.then((res) => res.data["access_token"])
-		.then((_token) => {
-			console.log("My token", token);
-			token = _token;
-			res.json({ ok: 1 });
-		})
-		.catch((error) => res.status(500).json({ message: error.message }));
+	const gitToken = async () => {
+		try {
+			const res = await axios.post(
+				`https://github.com/login/access_token`,
+				body,
+				opts
+			);
+			console.log(res.data);
+            return res.data
+		} catch (error) {
+			console.error(error);
+		}
+	};
 });
 
 module.exports = router;
