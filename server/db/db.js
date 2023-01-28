f8ecaad3859cfc14e211ba02544297da50128d2b
Bob <robert.dale.boles@gmail.com>
2023-01-28 14:51:03 -0700

    added events and interactivity

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,7 @@
 const Sequelize = require("sequelize");
 
 // const db = new Sequelize('postgres://localhost:5432/gitgoingdb', {logging:false})
+
 const db = new Sequelize("postgres", "postgres", `***REMOVED***`, {
     dialect: "postgres",
     host: "***REMOVED***",
