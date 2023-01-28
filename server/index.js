ed85e7d3d2264177de95ccf4f8fc6d2d3ed341da
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2023-01-27 22:41:48 -0500

    testing out env var

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 const {app} = require('./app');
 const port = 8080;
 
-console.log(process.env.TEST_ENV_VAR || "didn't work", "MAYBE TEST VAR")
 app.listen(process.env.PORT || port, ()=> console.log(`Listening ${process.env.PORT || port}`));
