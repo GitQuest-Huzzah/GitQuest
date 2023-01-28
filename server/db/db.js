27268afd71f32e1b6584dc1634015033d1f88eed
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2023-01-27 21:29:17 -0500

    cleaned up

--- server/db/db.js
+++ server/db/db.js
@@ -10,14 +10,6 @@ const db = new Sequelize("postgres", "postgres", `***REMOVED***`, {
     },
 });
 
-// db.authenticate()
-//     .then(() => {
-//         console.log("Connection has been established successfully.");
-//     })
-//     .catch((err) => {
-//         console.error("Unable to connect to the database:", err);
-//     });
-
 db.sync();
 
 module.exports = db;
