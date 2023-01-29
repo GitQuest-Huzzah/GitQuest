a67c0f170e413ac120d7671809c6e932aa8fb9cb
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2023-01-28 22:32:20 -0500

    jjjl

--- server/gitFuncs/commands.js
+++ server/gitFuncs/commands.js
@@ -2,18 +2,16 @@ const { Octokit } = require('@octokit/core');
 const { Users } = require('../db');
 
 const gitHubUserInfoAPI = async (reqBody) => {
-    console.log(reqBody, "req body of api call")
     const user = await Users.findOne({
         where:{
             slackID: reqBody.user_id
         }
     })
-    console.log(user, "user in api call")
     const octokit = new Octokit({
-        auth: user.gitHubToken
+        auth: user.dataValues.gitHubToken
     })
-    console.log(octokit, "octokit")
-    
+    console.log(octokit.request,"octokit request")
+    console.log(octokit.request.users,"request users octokit")
     const {data: {login}} = await octokit.request.users.getAuthenticated();
     console.log("hello, %s", login);
 }
