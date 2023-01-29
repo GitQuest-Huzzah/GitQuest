ec9d6b86de2f16c9e36e5f560931e89f4a7e59b7
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2023-01-29 16:05:50 -0500

    setting up secret manager

--- server/index.js
+++ server/index.js
@@ -1,26 +1,13 @@
-const {SecretManagerServiceClient} = require('@google-cloud/secret-manager').v1;
 const {app} = require('./app');
 const port = 8080;
 
 app.listen(process.env.PORT || port, ()=> console.log(`Listening ${process.env.PORT || port}`));
 
-
-async function main() {
-
-  const name = 'projects/1003391217227/secrets/ENV_VARIABLES/versions/1';
-  const {SecretManagerServiceClient} = require('@google-cloud/secret-manager');
-
-  const client = new SecretManagerServiceClient();
-
-  async function accessSecretVersion() {
-    const [version] = await client.accessSecretVersion({
-      name: name,
-    });
-    const payload = version.payload.data.toString();
-
-    console.log(`Payload: ${payload}`);
-  }
-  accessSecretVersion();
-}
-
-main().catch(console.error);
+if(process.env.DB_NAME) console.log("db name exists", process.env.DB_NAME)
+if(process.env.DB_USER) console.log("db user exists", process.env.DB_USER)
+if(process.env.DB_PASSWORD) console.log("db password exists", process.env.DB_PASSWORD)
+if(process.env.DB_CONNECTION) console.log("db connection exists", process.env.DB_CONNECTION)
+if(process.env.SLACK_CLIENT_ID) console.log("slack ID exists", process.env.SLACK_CLIENT_ID)
+if(process.env.SLACK_CLIENT_SECRET) console.log("slack secret exists", process.env.SLACK_CLIENT_SECRET)
+if(process.env.GH_CLIENT_SECRET) console.log("gh secret exists", process.env.GH_CLIENT_SECRET)
+if(process.env.GH_CLIENT_ID) console.log("gh ID exists", process.env.GH_CLIENT_ID)
