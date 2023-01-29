cfc9359ad9bc9c2a5d915ba360bb3115506952ef
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2023-01-29 14:41:25 -0500

    setting up secret manager

--- server/index.js
+++ server/index.js
@@ -5,20 +5,28 @@ const port = 8080;
 app.listen(process.env.PORT || port, ()=> console.log(`Listening ${process.env.PORT || port}`));
 
 
+async function main(name = 'projects/1003391217227/secrets/ENV_VARIABLES') {
 
-function main() {
-  const name = 'projects/1003391217227/secrets/ENV_VARIABLES'
+  const name = 'projects/1003391217227/secrets/ENV_VARIABLES';
 
-  const secretmanagerClient = new SecretManagerServiceClient();
+  // Imports the Secret Manager library
+  const {SecretManagerServiceClient} = require('@google-cloud/secret-manager');
 
-  async function callGetSecret() {
-    const request = {
-      name,
-    };
+  // Instantiates a client
+  const client = new SecretManagerServiceClient();
 
-    const response = await secretmanagerClient.getSecret(request);
-    console.log("this is the response from secret manager", response, 'seceret manager response');
+  async function getSecret() {
+    const [secret] = await client.getSecret({
+      name: name,
+    });
+
+    const policy = secret.replication.replication;
+
+    console.info(`Found secret ${secret.name} (${policy})`);
   }
-  callGetSecret();
+
+  getSecret();
 }
-main();
+
+const args = process.argv.slice(2);
+main(...args).catch(console.error);
