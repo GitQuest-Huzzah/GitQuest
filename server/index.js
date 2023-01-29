3c8437ef1a1c68c965e0166d1773335d0881faa1
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2023-01-28 21:45:53 -0500

    envVarAttempt

--- server/index.js
+++ server/index.js
@@ -1,4 +1,35 @@
+const {SecretManagerServiceClient} = require('@google-cloud/secret-manager').v1;
 const {app} = require('./app');
 const port = 8080;
 
 app.listen(process.env.PORT || port, ()=> console.log(`Listening ${process.env.PORT || port}`));
+
+
+
+function main(name) {
+  const name = 'projects/1003391217227/secrets/ENV_VARIABLES'
+
+  // Imports the Secretmanager library
+
+  // Instantiates a client
+  const secretmanagerClient = new SecretManagerServiceClient();
+
+  async function callGetSecret() {
+    // Construct request
+    const request = {
+      name,
+    };
+
+    // Run request
+    const response = await secretmanagerClient.getSecret(request);
+    console.log(response);
+  }
+
+  callGetSecret();
+}
+
+process.on('unhandledRejection', err => {
+  console.error(err.message);
+  process.exitCode = 1;
+});
+main(...process.argv.slice(2));
