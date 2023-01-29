bed3a7a191ce79d776f3e711a1ca00adefed3dd8
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2023-01-29 16:49:58 -0500

    setting up secret manager

--- server/index.js
+++ server/index.js
@@ -3,12 +3,12 @@ const port = 8080;
 
 app.listen(process.env.PORT || port, ()=> console.log(`Listening ${process.env.PORT || port}`));
 
-if(process.env.PORT) console.log('PORT EXISTS')
-if(process.env.DB_NAME) console.log("db name exists")
-if(process.env.DB_USER) console.log("db user exists")
-if(process.env.DB_PASSWORD) console.log("db password exists")
-if(process.env.DB_CONNECTION) console.log("db connection exists")
-if(process.env.SLACK_CLIENT_ID) console.log("slack ID exists")
-if(process.env.SLACK_CLIENT_SECRET) console.log("slack secret exists")
-if(process.env.GH_CLIENT_SECRET) console.log("gh secret exists")
-if(process.env.GH_CLIENT_ID) console.log("gh ID exists")
+// if(process.env.PORT) console.log('PORT EXISTS')
+// if(process.env.DB_NAME) console.log("db name exists")
+// if(process.env.DB_USER) console.log("db user exists")
+// if(process.env.DB_PASSWORD) console.log("db password exists")
+// if(process.env.DB_CONNECTION) console.log("db connection exists")
+// if(process.env.SLACK_CLIENT_ID) console.log("slack ID exists")
+// if(process.env.SLACK_CLIENT_SECRET) console.log("slack secret exists")
+// if(process.env.GH_CLIENT_SECRET) console.log("gh secret exists")
+// if(process.env.GH_CLIENT_ID) console.log("gh ID exists")
