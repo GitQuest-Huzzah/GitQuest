8df14d5fc8fa5b6bd1638a51816e6dada511843e
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2023-01-29 13:28:26 -0500

    setting up secret manager

--- server/api/github.js
+++ server/api/github.js
@@ -6,23 +6,13 @@ const router = require("express").Router();
 const githubClientId = "***REMOVED***";
 const githubClientSecret = "***REMOVED***";
 
-//path is /api/github/auth/connect
-// router.get("/auth/connect", async (req, res, next) => {
-// 	console.log("connect route hit");
-// 	res.redirect(
-// 		`https://github.com/login/oauth/authorize?client_id=${githubClientId}&scope=repo,repo:status,read:repo_hook,read:org,read:user,read:email,read:discussion`
-// 	);
-// });
-
 //path is /api/github/auth/redirect
 router.get("/auth/redirect", (req, res, next) => {
 	res.json({ git: "authorized" });
-	console.log(req.query, "redirected query");
 
 	const buffer64Obj = Buffer.from(req.query.state, "base64");
 	const decodedString = buffer64Obj.toString("utf8");
 	const parsedUserInfo = JSON.parse(decodedString);
-	console.log("decoded userID", parsedUserInfo);
 
 	const body = {
 		client_id: githubClientId,
--- server/gitFuncs/commands.js
+++ server/gitFuncs/commands.js
@@ -10,14 +10,10 @@ const gitHubUserInfoAPI = async (reqBody) => {
     const octokit = new Octokit({
         auth: user.dataValues.gitHubToken
     })
-    // const {data: {login}} = await octokit.rest.users.getAuthenticated();
-    // console.log("hello, %s", login);
-    // const gitHubUserAPIResponse = await octokit.request('GET /user', {})
     const gitHubCommits = await octokit.request('GET /repos/{owner}/{repo}/commits', {
         owner: 'GitQuest-Huzzah',
         repo: 'GitQuest'
       })
-    console.log("big ol new repos", gitHubCommits, "big ol new repos")
 }
 
 
