fa34c25155c91cf4460b1e20e3a2b5874cedd7bd
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2023-01-29 13:38:57 -0500

    setting up secret manager

--- server/index.js
+++ server/index.js
@@ -8,18 +8,14 @@ app.listen(process.env.PORT || port, ()=> console.log(`Listening ${process.env.P
 
 function main() {
   const name = 'projects/1003391217227/secrets/ENV_VARIABLES/versions/1'
-  // Imports the Secretmanager library
 
-  // Instantiates a client
   const secretmanagerClient = new SecretManagerServiceClient();
 
   async function callGetSecret() {
-    // Construct request
     const request = {
       name,
     };
 
-    // Run request
     const response = await secretmanagerClient.accessSecretVersion(request);
     console.log(response);
   }
