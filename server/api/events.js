4c0bad3ad165bfd1619c5a1dc341685d737a4c2a
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2023-01-30 14:47:49 -0500

    added comments to all files

--- server/api/events.js
+++ server/api/events.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 
+//path is api/events
+//this is listening to subscribed events on the workspace
 router.post("/", (req, res, next) => {
     console.log(req.body);
     res.send({code : "HTTP 200", challenge : req.body.challenge});
--- server/api/install.js
+++ server/api/install.js
@@ -2,11 +2,7 @@ const { slackInstallAuth } = require("../slackFuncs/commands");
 
 const router = require("express").Router();
 
-//path is /api/slack/install
-router.get("/", async (req, res, next) => {
-	res.sendStatus(200)
 
-});
 //path is /api/slack/install/redirect
 router.get("/redirect", (req,res) =>{
     slackInstallAuth(req);
