3049def90d55eb4e94dc75f1a4be419e22aea06c
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2023-01-30 14:47:49 -0500

    added comments to all files

--- server/app.js
+++ server/app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
 
+//json parsing middleware
 app.use(express.json());
 
+//url param decoding middleware
 app.use(express.urlencoded({extended:false}));
 
+//link app to app routes
 app.use("/api", require('./api'));
 
+//app error handling
 app.use((error, req, res, next) =>{
     console.error(error);
     res.status(error.status).send(error.message);
