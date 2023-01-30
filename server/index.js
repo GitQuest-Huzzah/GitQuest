52a2ef94a99417806b6d0dc4ad35a67a80a8e93b
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2023-01-29 23:56:07 -0500

    added basic readme

--- server/index.js
+++ server/index.js
@@ -12,28 +12,9 @@ const {SecretManagerServiceClient} = require('@google-cloud/secret-manager');
     });
     const payload = version.payload.data.toString('utf-8');
     const parsedPayload = JSON.parse(payload)
+    
+    console.log(JSON.stringify(parsedPayload), "stringed")
     console.log(parsedPayload.DB_NAME, "db name parsed")
-    // const parsedPayload = JSON.parse(payload)
-    process.env.DB_NAME = payload.DB_NAME
-    process.env.DB_PASSWORD = payload.DB_PASSWORD
-    process.env.DB_USER = payload.DB_USER
-    process.env.DB_CONNECTION = payload.DB_CONNECTION
-    console.log(process.env.DB_NAME, "did this work?")
-    // console.log(parsedPayload, "payload")
+
     app.listen(process.env.PORT || port, ()=> console.log(`Listening ${process.env.PORT || port}`));
 })();
-
-// function getSecret() {
-//     return client
-//       .accessSecretVersion({
-//         name: name,
-//       })
-//       .then((response) => response[0].payload.data.toString("utf-8"))
-//   }
-// accessSecretVersion()
-// const payload = getSecret()
-// console.log(payload,'payload')
-console.log(process.env.DB_NAME, "db name env")
-console.log(process.env.DB_PASSWORD, "db pass env")
-console.log(process.env.DB_USER, "db user env")
-console.log(process.env.DB_CONNECTION, "db conn env")
