1af34d715d0eda8d48a9ad6a0b9097dce83b5c5f
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2023-01-31 12:56:25 -0500

    testing webhook

--- server/gitFuncs/commands.js
+++ server/gitFuncs/commands.js
@@ -1,45 +1,64 @@
-const { Octokit } = require('@octokit/core');
-const { Users } = require('../db');
+const { Octokit } = require("@octokit/core");
+const { Users } = require("../db");
 
 //find a user's slackID to supply the GH API with access Token
 //the reqBody is the req.body from a slack command received at it's associated endpoint
 const gitHubUserInfoAPI = async (reqBody) => {
-    const user = await Users.findOne({
-        where:{
-            slackID: reqBody.user_id
-        }
-    })
+	const user = await Users.findOne({
+		where: {
+			slackID: reqBody.user_id,
+		},
+	});
 
-//creating a new instance of the GH API to poll the API which takes GH token as Auth headers
-    const octokit = new Octokit({
-        auth: user.dataValues.gitHubToken
-    })
+	//creating a new instance of the GH API to poll the API which takes GH token as Auth headers
+	const octokit = new Octokit({
+		auth: user.dataValues.gitHubToken,
+	});
 
-//request to get all commits from a specific repo
-    const gitHubCommits = await octokit.request('GET /repos/{owner}/{repo}/commits', {
-        owner: 'GitQuest-Huzzah',
-        repo: 'GitQuest'
-      })
-}
-  const gitHubSetRepoHook = async (reqBody) =>{
+	//request to get all commits from a specific repo
+	const gitHubCommits = await octokit.request(
+		"GET /repos/{owner}/{repo}/commits",
+		{
+			owner: "GitQuest-Huzzah",
+			repo: "GitQuest",
+		}
+	);
+};
+const gitHubSetRepoHook = async (reqBody) => {
+	const octokit = new Octokit({
+		auth: "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",
+	});
 
-     const octokit = new Octokit({ auth: "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"})
+	await octokit.request("POST /repos/{owner}/{repo}/hooks", {
+		owner: reqBody.owner,
+		repo: reqBody.repo,
+		name: "web",
+		active: true,
+		events: ["push", "pull_request"],
+		config: {
+			url: "https://gitquest.fun/api/webhook",
+			content_type: "json",
+			insecure_ssl: "0",
+		},
+	});
+};
 
-      await octokit.request('POST /repos/{owner}/{repo}/hooks', {
-          owner: reqBody.owner,
-          repo: reqBody.repo,
-          name: 'web',
-          active: true,
-          events: [
-              'push',
-              'pull_request'
-            ],
-            config: {
-                url: 'https://gitquest.fun/api/webhook',
-                content_type: 'json',
-                insecure_ssl: '0'
-            }
-        })
-    }
+const getAllOrgRepos = async (reqBody) => {
+	const octokit = new Octokit({
+		auth: "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",
+	});
+	const organization = await Users.findOne({
+		where: {
+			slackID: reqBody.user_id,
+		},
+		include: {
+			model: Workspaces,
+		},
+	});
+	const orgGitHubRepos = await octokit.request("GET /orgs/{owner}/{repo}/", {
+		owner: "GitQuest-Huzzah",//these need to be set to receive from the found user
+		repo: "GitQuest",
+	});
+};
 
-module.exports = {gitHubUserInfoAPI, gitHubSetRepoHook}
+module.exports = { gitHubUserInfoAPI, gitHubSetRepoHook };
