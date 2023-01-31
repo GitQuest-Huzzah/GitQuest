4cb12326519dc3035a8f380f18b3ef0dfb807676
Bob <robert.dale.boles@gmail.com>
2023-01-30 20:57:09 -0700

    added home func

--- server/index.js
+++ server/index.js
@@ -6,28 +6,28 @@ const { SecretManagerServiceClient } = require("@google-cloud/secret-manager");
 
 (async () => {
 	//name is the resource value for our secrets in secret manager
-	const name = "projects/1003391217227/secrets/ENV_VARIABLES/versions/13";
-	//instantiated a client for secretManager and calling accessSecretVersion method supplying the secret resource name
-	const client = new SecretManagerServiceClient();
-	const [version] = await client.accessSecretVersion({
-		name: name,
-	});
+	//const name = "projects/1003391217227/secrets/ENV_VARIABLES/versions/13";
+	////instantiated a client for secretManager and calling accessSecretVersion method supplying the secret resource name
+	//const client = new SecretManagerServiceClient();
+	//const [version] = await client.accessSecretVersion({
+	//	name: name,
+	//});
 
-	//turning the buffer value of the payload into a readable string
-	const payload = version.payload.data.toString("utf-8");
-    //parsing the payload string to get a JSON object
-	const parsedPayload = JSON.parse(payload);
+	////turning the buffer value of the payload into a readable string
+	//const payload = version.payload.data.toString("utf-8");
+    ////parsing the payload string to get a JSON object
+	//const parsedPayload = JSON.parse(payload);
 	
-	//assigning the associated key value pairs from the payload to env variables
-	//these are now accessible anywhere in the project
-	process.env.DB_CONNECTION = parsedPayload.DB_CONNECTION;
-	process.env.DB_NAME = parsedPayload.DB_NAME;
-	process.env.DB_PASSWORD = parsedPayload.DB_PASSWORD;
-	process.env.DB_USER = parsedPayload.DB_USER;
-	process.env.GITHUB_CLIENT_ID = parsedPayload.GITHUB_CLIENT_ID;
-	process.env.GITHUB_CLIENT_SECRET = parsedPayload.GITHUB_CLIENT_SECRET;
-	process.env.SLACK_CLIENT_ID = parsedPayload.SLACK_CLIENT_ID;
-	process.env.SLACK_CLIENT_SECRET = parsedPayload.SLACK_CLIENT_SECRET;
+	////assigning the associated key value pairs from the payload to env variables
+	////these are now accessible anywhere in the project
+	//process.env.DB_CONNECTION = parsedPayload.DB_CONNECTION;
+	//process.env.DB_NAME = parsedPayload.DB_NAME;
+	//process.env.DB_PASSWORD = parsedPayload.DB_PASSWORD;
+	//process.env.DB_USER = parsedPayload.DB_USER;
+	//process.env.GITHUB_CLIENT_ID = parsedPayload.GITHUB_CLIENT_ID;
+	//process.env.GITHUB_CLIENT_SECRET = parsedPayload.GITHUB_CLIENT_SECRET;
+	//process.env.SLACK_CLIENT_ID = parsedPayload.SLACK_CLIENT_ID;
+	//process.env.SLACK_CLIENT_SECRET = parsedPayload.SLACK_CLIENT_SECRET;
 
 	//finally we start the server
 	app.listen(process.env.PORT || port, () =>
