34be1ef6bb5c35ab7815565874602e2e070b75f4
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2023-01-31 18:59:20 -0500

    the selector now shows the options but only hardcoded options

--- server/api/selectMenus.js
+++ server/api/selectMenus.js
@@ -4,25 +4,40 @@ const router = require("express").Router();
 
 //path is api/selectMenus
 router.post("/", (req, res, next) => {
-	res.sendStatus(200);
+	// res.sendStatus(200);
 	const parsedSubmission = JSON.parse(req.body.payload);
 	(async () => {
 		const {
 			dataValues: { repos },
 		} = await findAllWorkSpaceRepos(parsedSubmission.user.team_id);
-		console.log(repos, "console log");
-		return {
-			options: repos.map((repo) => {
-							text: {
-								type: "plain_text",
-								text: "*this is plain_text text*",
-							},
-							value: "value-0",
-						
-				}
-            )
-		};
+		const names = repos.map((repo) => repo.dataValues.repoName);
 	})();
+	const options = {
+          "options": [
+            {
+              "text": {
+                  "type": "plain_text",
+                  "text": "*this is plain_text text*"
+              },
+              "value": "value-0"
+            },
+            {
+              "text": {
+                  "type": "plain_text",
+                  "text": "*this is plain_text text*"
+              },
+              "value": "value-1"
+            },
+            {
+              "text": {
+                  "type": "plain_text",
+                  "text": "*this is plain_text text*"
+              },
+              "value": "value-2"
+            }
+          ]
+        }
+	res.send(options).status(200);
 });
 
 module.exports = router;
