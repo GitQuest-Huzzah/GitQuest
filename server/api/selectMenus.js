bfc96234bd7151377e9fa37390e87a8f795dbbce
Bob <robert.dale.boles@gmail.com>
2023-01-31 22:48:45 -0700

    added drop down func, still needs to be handled at submit for adding webhooks.

--- server/api/selectMenus.js
+++ server/api/selectMenus.js
@@ -4,41 +4,48 @@ const router = require("express").Router();
 
 //path is api/selectMenus
 router.post("/", (req, res, next) => {
-	// res.sendStatus(200);
-	const parsedSubmission = JSON.parse(req.body.payload);
-	(async () => {
-		const {
-			dataValues: { repos },
-		} = await findAllWorkSpaceRepos(parsedSubmission.user.team_id);
-		const repoNames = repos.map((repo) => repo.dataValues.repoName);
-        console.log(repoNames)
-	})();
-	const options = {
-          "options": [
-            {
-              "text": {
-                  "type": "plain_text",
-                  "text": "*this is plain_text text*"
-              },
-              "value": "value-0"
-            },
-            {
-              "text": {
-                  "type": "plain_text",
-                  "text": "*this is plain_text text*"
-              },
-              "value": "value-1"
-            },
-            {
-              "text": {
-                  "type": "plain_text",
-                  "text": "*this is plain_text text*"
-              },
-              "value": "value-2"
+    // res.sendStatus(200);
+    const parsedSubmission = JSON.parse(req.body.payload);
+    // console.log(parsedSubmission)
+    (async () => {
+        const {
+            dataValues: { repos },
+        } = await findAllWorkSpaceRepos(parsedSubmission.user.team_id);
+        const repoNames = repos.map((repo) => repo.dataValues.repoName);
+        for (const repo of repoNames) {
+            if (
+                repo.includes(parsedSubmission.value) &&
+                parsedSubmission.value.length
+            ) {
+                const options = {
+                    options: [
+                        {
+                            text: {
+                                type: "plain_text",
+                                text: `${repo}`,
+                            },
+                            value: `${repo}`,
+                        },
+                    ],
+                };
+                return res.send(options).status(200);
+            } else {
+                const optionsArray = repoNames.reduce((acc, repo) => {
+                    let currentrepo = {
+                        text: {
+                            type: "plain_text",
+                            text: `${repo}`,
+                        },
+                        value: `${repo}`,
+                    };
+                    acc.push(currentrepo);
+                    return acc;
+                }, []);
+                res.send({ options: optionsArray }).status(200);
+                break;
             }
-          ]
         }
-	res.send(options).status(200);
+    })();
 });
 
 module.exports = router;
