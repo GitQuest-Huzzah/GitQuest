44c7aaf7f97c75e26cee3f9cf09926f211db9f1a
taylorrace <taylor@elevatestpete.fit>
2023-02-02 17:54:46 -0500

    completed funcs for user update. Todo: export func

---
+++ server/webhookFuncs/pullRequestAchievements.js
@@ -0,0 +1,9 @@
+const pullRequestAchievements = () => {
+  return {
+    1: "Congratulations, young neophyte. You've made your first pull request!",
+    5: "5 pull requests in the bag! Enjoy a level-up and riches to share.",
+    10: "You are rapidly gaining attention on your quest. 10 pull requests made!"
+  }
+}
+
+module.exports = pullRequestAchievements
