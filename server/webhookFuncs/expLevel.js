799ff0a1c3e723beeb49d92a9675b081657c4299
taylorrace <taylor@elevatestpete.fit>
2023-02-02 17:54:46 -0500

    completed funcs for user update. Todo: export func

---
+++ server/webhookFuncs/expLevel.js
@@ -0,0 +1,10 @@
+const expLevel = () => {
+  // key is EXP points, value is level
+  return {
+    0: 1,
+    500: 2,
+    1000: 3
+  }
+};
+
+module.exports = expLevel;
---
+++ server/webhookFuncs/titles.js
@@ -0,0 +1,10 @@
+const titles = () => {
+  return {
+    1: 'Neophyte',
+    4: 'Fledgling',
+    7: 'Apprentice',
+    10: 'Trailblazer'
+  }
+};
+
+module.exports = titles;
