c721db48e665cb2c53b550d16a46f2aa33d299f7
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2023-02-01 23:26:18 -0500

    pushing up users are connected to github account now

--- server/externalSelectMenus/externalGitHubUserConnectSelectMenu.js
+++ server/externalSelectMenus/externalGitHubUserConnectSelectMenu.js
@@ -2,8 +2,22 @@ const { gitHubOrgAllUsers } = require("../gitFuncs")
 
 const externalGitHubUserConnectSelectMenu = async(reqBody) =>{
    const allMembers = await gitHubOrgAllUsers(reqBody)
-   const memberGHAccounts = allMembers.data.map((member)=> member.login)
+   const memberGHAccounts = allMembers.data.map((member)=> member)
    console.log(memberGHAccounts, "all member")
+   
+   const optionsArray = memberGHAccounts.reduce((acc, member) => {
+    console.log(member.login, member.id)
+    let currentLogin = {
+        text: {
+            type: "plain_text",
+            text: `${member.login}`,
+        },
+        value: `${member.id}`,
+    };
+    acc.push(currentLogin);
+    return acc;
+}, []);
+return { options: optionsArray }
 }
 
 module.exports = externalGitHubUserConnectSelectMenu;
