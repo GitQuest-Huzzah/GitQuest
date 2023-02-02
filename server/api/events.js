0bfc576774d3c6b0d92a8a90717f1fc8f986c58a
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2023-02-01 23:40:29 -0500

    pushing up users are connected to github account now

--- server/api/events.js
+++ server/api/events.js
@@ -4,9 +4,12 @@ const { homeTab } = require("../slackFuncs");
 //path is api/events
 //this is listening to subscribed events on the workspace
 router.post("/", (req, res, next) => {
-	if(req.body.challenge)res.send({code : "HTTP 200", challenge : req.body.challenge});
-	if (req.body.event.type === "app_home_opened") return homeTab(req.body);
-	res.sendStatus(200);
+	if (req.body.challenge)
+		res.send({ code: "HTTP 200", challenge: req.body.challenge });
+	if (req.body.event.type === "app_home_opened") {
+		res.sendStatus(200);
+		return homeTab(req.body);
+	}
 });
 
 module.exports = router;
