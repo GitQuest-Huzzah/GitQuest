0e9b3e75f1fb38e3b7c8f52ab10127c819fe1376
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2023-02-02 13:53:13 -0500

    fixing issue

--- server/slackFuncs/createAdminGHLink.js
+++ server/slackFuncs/createAdminGHLink.js
@@ -1,14 +1,15 @@
-const createAdminGHLink = (reqBody) =>{
-    const userInfo = JSON.stringify({
-        userId: reqBody.user_id,
-        teamId: reqBody.team_id,
-    });
-    const githubClientId = "***REMOVED***";
-    //we are turning the string into a buffer
+const createAdminGHLink = ({ teamId, userId }) => {
+	const userInfo = JSON.stringify({
+		userId: userId,
+		teamId: teamId,
+	});
+	// console.log(reqBody.event)
+	const githubClientId = "***REMOVED***";
+	//we are turning the string into a buffer
 	const bufferUTFObj = Buffer.from(userInfo, "utf8");
 	//this transforms the buffer into a base64 string before sending it so the user in the link on the optional state parameter
 	const base64String = bufferUTFObj.toString("base64");
-    return `https://github.com/login/oauth/authorize?client_id=${githubClientId}&scope=repo,read:status,read:repo_hook,read:org,read:user,read:email,read:discussion&state=${base64String}`
-}
+	return `https://github.com/login/oauth/authorize?client_id=${githubClientId}&scope=repo,read:status,read:repo_hook,read:org,read:user,read:email,read:discussion&state=${base64String}`;
+};
 
 module.exports = createAdminGHLink;
