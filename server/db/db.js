827179a6795152ede5f9aeaa8c98237ada1701c5
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2023-02-02 19:19:28 -0500

    Update db.js

--- server/db/db.js
+++ server/db/db.js
@@ -1,28 +1,18 @@
 const Sequelize = require("sequelize");
 
 //this is the connection to a localinstance of the DB
-let db
-if (process.env.ENV) {
-	db = new Sequelize(process.env.DB_CONNECTION_DEV, { logging: false });
-	
-}
-// this is the connection to the deployed DB
-if (process.env.DB_NAME) {
-	db = new Sequelize(
-		process.env.DB_NAME,
-		process.env.DB_USER,
-		process.env.DB_PASSWORD,
-		{
-			dialect: "postgres",
-			host: process.env.DB_CONNECTION,
-			logging: false,
-			dialectOptions: {
-				socketPath: process.env.DB_CONNECTION,
-			},
-		}
-	);
-	
-}
+// const db = new Sequelize('postgres://localhost:5432/gitgoingdb', {logging:false})
+//this is the connection to the deployed DB
+const db = new Sequelize("postgres", "postgres", `***REMOVED***`, {
+    dialect: "postgres",
+    host: "***REMOVED***",
+    logging: false,
+    dialectOptions: {
+        socketPath: "***REMOVED***",
+    },
+});
+
 db.sync();
 
-module.exports = db;
+module.exports = db
+
