7324f4be06d78a16795a8b43ea81d4b1be513f73
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2023-02-03 04:23:52 -0500

    commit before this everything works this is the first attempt with db wired up

--- server/db/db.js
+++ server/db/db.js
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV !== "production") {
 	});
 }
 if (process.env.NODE_ENV === "production") {
-	db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+	db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, `***REMOVED***`, {
 		dialect: "postgres",
 		host: process.env.DB_CONNECTION,
 		logging: false,
