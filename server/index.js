146f3dedec3088ec2917b51b7156f3f793c08506
Don Mallory <don.mallory@arcade.co>
2023-02-03 12:47:25 -0600

    Refactor credential fetching conditionally on environment type

--- server/index.js
+++ server/index.js
@@ -1,7 +1,14 @@
-const { app } = require("./app");
-require('dotenv').config();
-(async () => {
-	app.listen(process.env.PORT, () =>
-		console.log(`Listening ${process.env.PORT}`)
-	);
-})();
+const retrieveRemoteSecrets = require("./retrieveSecrets");
+require("dotenv").config();
+
+maybeLoadRemoteSecrets().then(() =>
+  require("./app").listen(process.env.PORT, () =>
+    console.log(`Listening ${process.env.PORT}`)
+  )
+);
+
+const maybeLoadRemoteSecrets = () => {
+  if (process.env.ENVIRONMENT === "development") return Promise.resolve();
+
+  return retrieveRemoteSecrets();
+};
