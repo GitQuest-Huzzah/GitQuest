c6e58d9f8165b189944c375d68c0960fa6e10848
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2023-02-03 00:00:27 -0500

    env attempt 2 i think it works

--- server/db/db.js
+++ server/db/db.js
@@ -1,25 +1,22 @@
 const Sequelize = require("sequelize");
 
-//this is the connection to DB
-const options = () => {
-	if (process.env.ENV === "DEV") {
-		console.log("hit 1");
-		return process.env.DB_CONNECTION;
-	}
-	return (
-		process.env.DB_NAME,
-		process.env.DB_USER,
-		process.env.DB_PASSWORD,
-		{
-			dialect: "postgres",
-			host: process.env.DB_CONNECTION,
-			logging: false,
-			dialectOptions: {
-				socketPath: process.env.DB_CONNECTION,
-			},
-		}
-	);
-};
-const db = new Sequelize(options());
+//this is the connection to a localinstance of the DB
+let db = "";
+if(process.env.ENV === "DEV"){
+db = new Sequelize('postgres://localhost:5432/gitgoingdb', {logging:false})
+}
+//this is the connection to the deployed DB
+if(process.env.ENV !== "DEV"){
+	db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+	   dialect: "postgres",
+	   host: process.env.DB_CONNECTION,
+	   logging: false,
+	   dialectOptions: {
+		   socketPath: process.env.DB_CONNECTION,
+	   },
+   });
+}
+
 db.sync();
+
 module.exports = db;
