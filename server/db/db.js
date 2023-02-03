ea358be7c5cefd3320cb3756e3d697a8e673c9ce
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2023-02-03 00:06:01 -0500

    env attempt 2 i think it works

--- server/db/db.js
+++ server/db/db.js
@@ -2,11 +2,11 @@ const Sequelize = require("sequelize");
 
 //this is the connection to a localinstance of the DB
 let db = "";
-if(process.env.ENV === "DEV"){
+if(process.env.DEV === "YESSIR"){
 db = new Sequelize('postgres://localhost:5432/gitgoingdb', {logging:false})
 }
 //this is the connection to the deployed DB
-if(process.env.ENV !== "DEV"){
+if(process.env.DEV !== "YESSIR"){
 	db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
 	   dialect: "postgres",
 	   host: process.env.DB_CONNECTION,
