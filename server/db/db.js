397e420c5fb76fed1fdb67f2112d6dc2294603bb
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2023-02-03 01:48:42 -0500

    env attempt 2 i think it works

--- server/db/db.js
+++ server/db/db.js
@@ -1,53 +1,24 @@
 const Sequelize = require("sequelize");
-const { SecretManagerServiceClient } = require("@google-cloud/secret-manager");
 
 //this is the connection to a localinstance of the DB
+console.log(process.env)
 let db = "";
-if(process.env.DEV !== "YES"){
-	
-	const client = new SecretManagerServiceClient();
-	client.accessSecretVersion({
-		name: "projects/1003391217227/secrets/ENV_VARIABLES/versions/13",
-	})
-	.then(([version]) => {
-		console.log("deploy route");
-		//turning the buffer value of the payload into a readable string
-		const payload = version.payload.data.toString("utf-8");
-		//parsing the payload string to get a JSON object
-		console.log(payload, "deploy payload pre parse");
-		const parsedPayload = JSON.parse(payload);
-		console.log(parsedPayload, "deploy payload");
-		//assigning the associated key value pairs from the payload to env variables
-		//these are now accessible anywhere in the project
-		process.env.DB_CONNECTION = parsedPayload.DB_CONNECTION;
-		process.env.DB_NAME = parsedPayload.DB_NAME;
-		process.env.DB_PASSWORD = parsedPayload.DB_PASSWORD;
-		process.env.DB_USER = parsedPayload.DB_USER;
-		process.env.GITHUB_CLIENT_ID = parsedPayload.GITHUB_CLIENT_ID;
-		process.env.GITHUB_CLIENT_SECRET = parsedPayload.GITHUB_CLIENT_SECRET;
-		process.env.SLACK_CLIENT_ID = parsedPayload.SLACK_CLIENT_ID;
-		process.env.SLACK_CLIENT_SECRET = parsedPayload.SLACK_CLIENT_SECRET;
-	})
-	.then(
-		(db = new Sequelize(
-			process.env.DB_NAME,
-			process.env.DB_USER,
-			process.env.DB_PASSWORD,
-			{
-				dialect: "postgres",
-				host: process.env.DB_CONNECTION,
-				logging: false,
-				dialectOptions: {
-					socketPath: process.env.DB_CONNECTION,
-				},
-			}
-			))
-			);
-		}
-			if (process.env.DEV === "YES") {
-	exports = db = new Sequelize("postgres://localhost:5432/gitgoingdb", {
-		logging: false,
-	});
+if(process.env.NODE_ENV !== "production"){
+db = new Sequelize('postgres://localhost:5432/gitgoingdb', {logging:false})
 }
+//this is the connection to the deployed DB
+if(process.env.NODE_ENV === "production"){
+	
+	db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+	   dialect: "postgres",
+	   host: process.env.DB_CONNECTION,
+	   logging: false,
+	   dialectOptions: {
+		   socketPath: process.env.DB_CONNECTION,
+	   },
+   });
+}
+
+db.sync();
 
 module.exports = db;
