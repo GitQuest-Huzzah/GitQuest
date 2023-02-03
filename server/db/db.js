6406640c54846c9d56e0dc4c24ba399403d7062e
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2023-02-03 02:26:52 -0500

    env attempt 2 i think it works

--- server/db/db.js
+++ server/db/db.js
@@ -1,30 +1,35 @@
+const { Model } = require("sequelize");
 const Sequelize = require("sequelize");
 const retrieveSecrets = require("../../retrieveSecret");
 
 //this is the connection to a localinstance of the DB
 console.log(process.env);
-if (process.env.NODE_ENV !== "production") {
-	exports = db = new Sequelize("postgres://localhost:5432/gitgoingdb", {
-		logging: false,
-	});
+const db = () =>{
 	
-}
-//this is the connection to the deployed DB
-if (process.env.NODE_ENV === "production") {
-	retrieveSecrets()
-		exports = db = new Sequelize(
-			process.env.DB_NAME,
-			process.env.DB_USER,
-			process.env.DB_PASSWORD,
-			{
-				dialect: "postgres",
-				host: process.env.DB_CONNECTION,
-				logging: false,
-				dialectOptions: {
-					socketPath: process.env.DB_CONNECTION,
-				},
-			}
-		)
-	
+	//this is the connection to the deployed DB
+	if (!process.env || process.env.NODE_ENV !== "production") {
+		return new Sequelize("postgres://localhost:5432/gitgoingdb", {
+			logging: false,
+		});
+		
 	}
-	// db.sync();
+	if (process.env.NODE_ENV === "production") {
+		retrieveSecrets()
+		return new Sequelize(
+			process.env.DB_NAME,
+				process.env.DB_USER,
+				process.env.DB_PASSWORD,
+				{
+					dialect: "postgres",
+					host: process.env.DB_CONNECTION,
+					logging: false,
+					dialectOptions: {
+						socketPath: process.env.DB_CONNECTION,
+					},
+				}
+			)
+		
+		}
+}
+
+module.exports=db()
