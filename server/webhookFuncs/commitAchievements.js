50122f83cf7a21334b3e1333666393fc9cfb8942
taylorrace <taylor@elevatestpete.fit>
2023-02-03 12:54:25 -0500

    incremented levels and dominated achievements with JSON magic

--- server/webhookFuncs/commitAchievements.js
+++ server/webhookFuncs/commitAchievements.js
@@ -1,10 +1,10 @@
 const commitAchievements = () => {
   return {
   1: "You've made your first commit!",
-  50: "Halfway to 100 commits. You are an aspiring adventurer.",
-  100: "100 Commits! Unbelievable...fortunes are coming your way, with extra to share.",
-  250: "You are becoming the conqueror of commits. 250 completed!",
-  500: "500 commits? 500! Your quest is well underway. Enjoy the spoils of your work.",
+  2: "Halfway to 100 commits. You are an aspiring adventurer.",
+  3: "100 Commits! Unbelievable...fortunes are coming your way, with extra to share.",
+  4: "You are becoming the conqueror of commits. 250 completed!",
+  5: "500 commits? 500! Your quest is well underway. Enjoy the spoils of your work.",
   750: "750 commits made. Your powers are growing (and so is your gold). Share the rewards with your clan.",
   1000: "The pinnacle of commits. ONE THOUSAND! A legend before our eyes!"
   }
