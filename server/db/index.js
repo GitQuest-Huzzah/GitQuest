c49debe1e475ec0d2ee5c0bef6a57cdc30f1e10d
Bob <robert.dale.boles@gmail.com>
2023-02-03 14:04:36 -0700

    agai

--- server/db/index.js
+++ server/db/index.js
@@ -2,6 +2,7 @@ const db = require("./db");
 const Repos = require("./models/Repos");
 const Users = require("./models/Users");
 const Workspaces = require("./models/Workspaces");
+const GoldLog = require('./models/GoldLog')
 
 //model associations
 
@@ -10,6 +11,9 @@ Workspaces.hasMany(Users);
 
 Repos.belongsTo(Workspaces);
 Workspaces.hasMany(Repos);
+
+Users.hasMany(GoldLog)
+GoldLog.belongsTo(Users)
 
 module.exports = {
   db,
