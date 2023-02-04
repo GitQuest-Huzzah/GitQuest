20a8c7f41527b99bceb304576f1857be5a6199e7
Bob <robert.dale.boles@gmail.com>
2023-02-04 14:32:44 -0700

    achievements hooked up

--- server/slackFuncs/achievementsModal.js
+++ server/slackFuncs/achievementsModal.js
@@ -1,31 +1,26 @@
 const { WebClient } = require("@slack/web-api");
 const findTokenByTeamId = require("./findTokenByTeam");
-const GoldLog = require("../db/models/Goldlog");
 const Users = require("../db/models/Users");
 //instantiating an instance of the slack Web Client API
 const web = new WebClient();
 
 const goldLogModal = async (reqBody) => {
     console.log(reqBody.user.id);
-    const goldLogs = await GoldLog.findAll({
-        include: {
-            model: Users,
+    const {dataValues:{achievements}} = await Users.findOne({
             where: {
                 slackID: reqBody.user.id,
             },
-        },
     });
-    console.log(goldLogs);
     const token = await findTokenByTeamId(reqBody.user.team_id);
     await web.views.open({
         trigger_id: reqBody.trigger_id,
         token: token,
         view: {
-            external_id: "goldLogModal",
+            external_id: "achievementsModal",
             type: "modal",
             title: {
                 type: "plain_text",
-                text: "Gold Log",
+                text: "Achievements",
                 emoji: true,
             },
             close: {
@@ -33,12 +28,12 @@ const goldLogModal = async (reqBody) => {
                 text: "Close",
                 emoji: true,
             },
-            blocks: goldLogs.map((log) => {
+            blocks: achievements.map((log) => {
                 return {
                     type: "section",
                     text: {
                         type: "mrkdwn",
-                        text: `${log.dataValues.description} ${log.dataValues.valueChange}` 
+                        text: `${Object.values(log)[0]}`
                     },
                 };
             }),
