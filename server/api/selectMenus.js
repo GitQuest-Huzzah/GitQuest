ddebb31b0bf8ee785c27e7a64dc6818b5738817c
Bob <robert.dale.boles@gmail.com>
2023-02-06 10:00:31 -0700

    users can now claim quests. TODO: add webhook func

--- server/api/selectMenus.js
+++ server/api/selectMenus.js
@@ -15,7 +15,7 @@ router.post("/", async (req, res, next) => {
 	if (parsedSubmission.view.external_id === "adminDeleteReposSubmit"){
 		res.send(await externalRepoDeleteSelectMenu(parsedSubmission)).status(200)
 	}
-	if (parsedSubmission.view.external_id === "questLogSubmit"){
+	if (parsedSubmission.view.callback_id=== "questLogSubmit"){
 		res.send(await externalQuestLogSelect(parsedSubmission)).status(200)
 	}
 });
