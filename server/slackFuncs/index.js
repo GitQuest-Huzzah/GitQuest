1ce38ec3fba563bdf84f47a2483615b301400979
Bob <robert.dale.boles@gmail.com>
2023-02-05 22:52:58 -0700

    added quest log. select menu drop down, claiming the quest, webhook handling still todo

--- server/slackFuncs/index.js
+++ server/slackFuncs/index.js
@@ -1,6 +1,6 @@
-const achievementsModal = require('./achievementsModal')
-const addQuest = require('./addQuest')
-const addQuestModal = require('./addQuestModal')
+const achievementsModal = require("./achievementsModal");
+const addQuest = require("./addQuest");
+const addQuestModal = require("./addQuestModal");
 const adminGitConnectUserModal = require("./adminGitConnectUserModal");
 const adminOrgModal = require("./adminOrgModal");
 const adminRepoModal = require("./adminRepoModal");
@@ -9,27 +9,31 @@ const createOrUpdateOrg = require("./createOrUpdateOrg");
 const findAllWorkSpaceRepos = require("./findAllWorkSpaceRepos");
 const findTokenByTeamId = require("./findTokenByTeam");
 const gitWorkFlow = require("./gitWorkFlow");
-const giveGold = require('./giveGold')
-const giveGoldModal = require("./giveGoldModal")
-const goldLogModal = require('./goldLogModal')
+const giveGold = require("./giveGold");
+const giveGoldModal = require("./giveGoldModal");
+const goldLogModal = require("./goldLogModal");
 const homeTab = require("./homeTab");
+const questLog = require("./questLog");
+const questLogModal = require("./questLogModal");
 const slackInstallAuth = require("./slackInstallAuth");
 
 module.exports = {
     achievementsModal,
     addQuest,
     addQuestModal,
-	adminGitConnectUserModal,
-	adminOrgModal,
-	adminRepoModal,
-	blockTest,
-	createOrUpdateOrg,
-	findAllWorkSpaceRepos,
-	findTokenByTeamId,
+    adminGitConnectUserModal,
+    adminOrgModal,
+    adminRepoModal,
+    blockTest,
+    createOrUpdateOrg,
+    findAllWorkSpaceRepos,
+    findTokenByTeamId,
     giveGoldModal,
-	gitWorkFlow,
+    gitWorkFlow,
     giveGold,
     goldLogModal,
-	homeTab,
-	slackInstallAuth,
+    homeTab,
+    questLog,
+    questLogModal,
+    slackInstallAuth,
 };
