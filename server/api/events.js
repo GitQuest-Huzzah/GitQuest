e5401e42987092f1e86ae6f0d4b5b36123a06d3b
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2023-02-06 19:04:19 -0500

    will profile modal show

--- server/api/events.js
+++ server/api/events.js
@@ -5,7 +5,8 @@ const { homeTab } = require("../slackFuncs");
 //this is listening to subscribed events on the workspace
 router.post("/", (req, res, next) => {
 	if (req.body.challenge)
-		res.send({ code: "HTTP 200", challenge: req.body.challenge });
+		return res.send({ code: "HTTP 200", challenge: req.body.challenge });
+		
 	if (req.body.event.type === "app_home_opened") {
 		res.sendStatus(200);
 		return homeTab(req.body);
