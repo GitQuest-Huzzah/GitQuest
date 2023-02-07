cec1f25487d74eec1c0d439d3c4cd5210b274cbf
Bob <robert.dale.boles@gmail.com>
2023-02-07 10:29:24 -0700

    admin buttons only show if you are an admin

---
+++ server/slackFuncs/userHomeView.js
@@ -0,0 +1,83 @@
+const userHomeView = (user) => {
+    return [
+        {
+            type: "image",
+            image_url: `https://storage.googleapis.com/slackphotos/gqbanner.png`,
+            alt_text: "Git Quest Banner",
+        },
+        {
+            type: "header",
+            text: {
+                type: "plain_text",
+                text: "Welcome Hero!",
+            },
+        },
+        {
+            type: "divider",
+        },
+        {
+            type: "section",
+            fields: [
+                {
+                    type: "mrkdwn",
+                    text: `*User Profile*\n*Level*: ${user.dataValues.level}\n*Title*:${user.dataValues.title} \n *Total Exp*: ${user.dataValues.exp}\n*Gold*: ${user.dataValues.gold} \n *Gold to Give*: ${user.dataValues.rewardGold}`,
+                },
+            ],
+        },
+        {
+            type: "divider",
+        },
+        {
+            type: "actions",
+            elements: [
+                {
+                    type: "button",
+                    text: {
+                        type: "plain_text",
+                        text: "Quest Log",
+                        emoji: true,
+                    },
+                    action_id: "questLogButton",
+                },
+                {
+                    type: "button",
+                    text: {
+                        type: "plain_text",
+                        text: "Give Gold to Give",
+                        emoji: true,
+                    },
+                    action_id: "giveGoldButton",
+                },
+                {
+                    type: "button",
+                    text: {
+                        type: "plain_text",
+                        text: "Gold Log",
+                        emoji: true,
+                    },
+                    action_id: "goldLogButton",
+                },
+                {
+                    type: "button",
+                    text: {
+                        type: "plain_text",
+                        text: "Achievements",
+                        emoji: true,
+                    },
+                    action_id: "achievementButton",
+                },
+                {
+                    type: "button",
+                    text: {
+                        type: "plain_text",
+                        text: "My Profile",
+                        emoji: true,
+                    },
+                    action_id: "profileButton",
+                },
+            ],
+        },
+    ];
+};
+
+module.exports = userHomeView;
