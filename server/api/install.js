e41906430e37710594f5bc399f5c1f471c1935fb
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2023-02-08 11:29:30 -0500

    take care of slackauthinstall edgecases changed fail return

--- server/api/install.js
+++ server/api/install.js
@@ -5,6 +5,7 @@ const router = require("express").Router();
 //path is /api/slack/install/redirect
 router.get("/redirect", (req,res) =>{
     const installed = slackInstallAuth(req);
+    console.log(installed, "is installed true or false?")
     installed ? res.status(200).redirect('https://gitquest.fun/faq?installed=successful') : res.status(400).redirect('https://gitquest.fun/faq?installed=failed')
 });
 
