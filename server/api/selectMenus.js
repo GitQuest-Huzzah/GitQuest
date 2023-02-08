155c9b0237fc50d6f72fd5225d36966fc5efcc2e
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2023-02-07 19:24:46 -0500

    major reorg

--- server/api/selectMenus.js
+++ server/api/selectMenus.js
@@ -1,5 +1,10 @@
-const {externalRepoSelectMenu, externalGitHubUserConnectSelectMenu, externalRepoDeleteSelectMenu, externalQuestLogSelect}  = require("../externalSelectMenus");
-
+const {
+	externalRepoDeleteSelectMenu,
+	externalGitHubUserConnectSelectMenu,
+	externalQuestSelect,
+	externalRepoSelectMenu,
+} = require("../../slackHomeTab/externalSelectMenus");
+externalRepoDeleteSelectMenu;
 const router = require("express").Router();
 
 //path is api/selectMenus
@@ -10,13 +15,15 @@ router.post("/", async (req, res, next) => {
 		res.send(await externalRepoSelectMenu(parsedSubmission)).status(200);
 	}
 	if (parsedSubmission.view.callback_id === "adminGitConnectUserSubmit") {
-		res.send(await externalGitHubUserConnectSelectMenu(parsedSubmission)).status(200);
+		res
+			.send(await externalGitHubUserConnectSelectMenu(parsedSubmission))
+			.status(200);
 	}
-	if (parsedSubmission.view.callback_id === "adminDeleteReposSubmit"){
-		res.send(await externalRepoDeleteSelectMenu(parsedSubmission)).status(200)
+	if (parsedSubmission.view.callback_id === "adminDeleteReposSubmit") {
+		res.send(await externalRepoDeleteSelectMenu(parsedSubmission)).status(200);
 	}
-	if (parsedSubmission.view.callback_id=== "viewQuestsSubmit"){
-		res.send(await externalQuestLogSelect(parsedSubmission)).status(200)
+	if (parsedSubmission.view.callback_id === "viewQuestsSubmit") {
+		res.send(await externalQuestSelect(parsedSubmission)).status(200);
 	}
 });
 
--- server/slackFuncs/viewQuestBlockFunc.js
+++
@@ -1,4 +0,0 @@
-const viewQuestBlockFunc = (quests) => {
-};
-
-module.exports = viewQuestBlockFunc;
