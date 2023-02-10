8f0f3f96b82e0c666e614022682ed66c03707857
Bob <robert.dale.boles@gmail.com>
2023-02-10 13:20:57 -0700

    achievements /w quests functional

--- helperFuncs/webhookFuncs/userAchievement.js
+++ helperFuncs/webhookFuncs/userAchievement.js
@@ -1,70 +1,24 @@
 const { Achievement, Users } = require("../../server/db");
+const updateAchievement = require("./updateAchievement");
 
-const userAchievement = async (user, commits, pulls, numQuestCompleted) => {
+const userAchievement = async (user, commits, pulls) => {
     const achievements = await Achievement.findAll();
 
-    console.log(
-        user.achievements[0].user_achievement,
-        "THIS IS THE USER IN THROUGH TABLES"
+    const commitAchieve = updateAchievement(
+        commits,
+        "commitValue",
+        achievements
     );
-    console.log(
-        user.dataValues.achievements,
-        "THIS IS ON THE USER IN DATAVALUES"
+    const pullAchieve = updateAchievement(
+        pulls,
+        "pullRequestValue",
+        achievements
     );
 
-    //find the highest achievement that the user should have, and then compare that to the current achievement of the user. If they have it, do nothing, if they don't, add it to their current achievements
-
-    let commitAchieve;
-    let pullAchieve;
-    let questAchieve;
-
-    const findHighestAchievement = (commits, pulls, numQuestCompleted) => {
-        for (const achievement of achievements) {
-            console.log(
-                achievement,
-                "this is achievement passed in the for loop"
-            );
-            if (
-                commits >= achievement.dataValues.commitValue &&
-                achievement.dataValues.commitValue
-            ) {
-                commitAchieve = achievement.dataValues.id;
-            }
-        }
-        for (const achievement of achievements) {
-            console.log(
-                achievement,
-                "this is achievement passed in the for loop"
-            );
-            if (
-                pulls >= achievement.dataValues.pullRequestValue &&
-                achievement.dataValues.pullRequestValue
-            ) {
-                pullAchieve = achievement.dataValues.id;
-            }
-        }
-        for (const achievement of achievements) {
-            console.log(
-                achievement,
-                "this is achievement passed in the for loop"
-            );
-            if (
-                numQuestCompleted >= achievement.dataValues.questsCompleted &&
-                achievement.dataValues.questsCompleted
-            ) {
-                questAchieve = achievement.dataValues.id;
-            }
-        }
-        const result = [pullAchieve, commitAchieve, numQuestCompleted];
-        console.log(result);
-        user.addAchievements(
-            result.filter((achievement) => (achievement ? true : false))
-        );
-    };
-
-    findHighestAchievement(commits, pulls, numQuestCompleted);
-    console.log(commits, "This is commits coming into this function");
-    console.log(commitAchieve, "This is the achievement ID");
+    const result = [pullAchieve, commitAchieve];
+    user.addAchievements(
+        result.filter((achievement) => (achievement ? true : false))
+    );
 };
 
 module.exports = userAchievement;
