97040aa44490618bac7e17acc0ee0e6ab142dac4
Bob <robert.dale.boles@gmail.com>
2023-02-16 12:04:46 -0700

    update id on admin completed

--- helperFuncs/slackFuncs/adminAssignQuestComplete.js
+++ helperFuncs/slackFuncs/adminAssignQuestComplete.js
@@ -3,61 +3,62 @@ const userLevelFunc = require("../webhookFuncs/userLevelFunc");
 const updateAchievement = require("../webhookFuncs/updateAchievement");
 
 const adminAssignQuestComplete = async (reqBody) => {
-	const {
-		view: {
-			state: {
-				values: { questBlock, userSelectBlock },
-			},
-		},
-	} = reqBody;
+    const {
+        view: {
+            state: {
+                values: { questBlock, userSelectBlock },
+            },
+        },
+    } = reqBody;
 
-	const quests = questBlock.questAction.selected_options;
-	const userId = userSelectBlock.userSelectAction.selected_user;
-	const user = await User.findOne({
-		where: {
-			slackID: userId,
-		},
-		include: {
-			model: Playerstat,
-		},
-	});
+    const quests = questBlock.questAction.selected_options;
+    const userId = userSelectBlock.userSelectAction.selected_user;
+    const user = await User.findOne({
+        where: {
+            slackID: userId,
+        },
+        include: {
+            model: Playerstat,
+        },
+    });
 
-	if (user && quests.length) {
-		const achievements = await Achievement.findAll();
-		quests.forEach(async (quest) => {
-			const questsCompleted =
-				user.dataValues.playerstat.dataValues.questsCompleted + 1;
-			const achievement = await updateAchievement(
-				questsCompleted,
-				"questsCompleted",
-				achievements
-			);
-			user.addAchievement(achievement);
-			const singleQuest = await Quest.findOne({
-				where: {
-					id: quest.value,
-				},
-			});
-			Playerstat.update(
-				{
-					questsCompleted,
-				},
-				{
-					where: {
-						userId: user.dataValues.id,
-					},
-				}
-			);
-			userLevelFunc(
-				user,
-				singleQuest.dataValues.expValue,
-				singleQuest.dataValues.goldValue
-			);
-			singleQuest.update({
-				status: "completed",
-			});
-		});
-	}
+    if (user && quests.length) {
+        const achievements = await Achievement.findAll();
+        quests.forEach(async (quest) => {
+            const questsCompleted =
+                user.dataValues.playerstat.dataValues.questsCompleted + 1;
+            const achievement = await updateAchievement(
+                questsCompleted,
+                "questsCompleted",
+                achievements
+            );
+            user.addAchievement(achievement);
+            const singleQuest = await Quest.findOne({
+                where: {
+                    id: quest.value,
+                },
+            });
+            Playerstat.update(
+                {
+                    questsCompleted,
+                },
+                {
+                    where: {
+                        userId: user.dataValues.id,
+                    },
+                }
+            );
+            userLevelFunc(
+                user,
+                singleQuest.dataValues.expValue,
+                singleQuest.dataValues.goldValue
+            );
+            singleQuest.update({
+                status: "completed",
+                userId: user.dataValues.id
+            });
+        });
+    }
 };
 
 module.exports = adminAssignQuestComplete;
