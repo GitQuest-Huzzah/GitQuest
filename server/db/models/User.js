dc592f1baf88b341c3de3044c93521e7e092dadd
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2023-04-07 11:21:47 -0400

    no case sensitive email'

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -43,8 +43,7 @@ User.prototype.correctPassword = function (candidatePwd) {
 };
 
 User.authenticate = async function ({ email, password }) {
-    const emailLower = email.toLowerCase()
-    const user = await this.findOne({ where: { emailLower } });
+    const user = await this.findOne({ where: { email } });
     if (!user || !(await user.correctPassword(password))) {
         const error = Error("Incorrect Email / Password");
         error.status = 401;
